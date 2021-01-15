caaba12f13a9033e99393a9f1ede2a9330290452
taylulz <taylulzcode@gmail.com>
2021-01-15 15:12:52 -0800

    add all treats and flavors to splash page

--- TreatBox/Controllers/HomeController.cs
+++ TreatBox/Controllers/HomeController.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.Collections.Generic;
+using TreatBox.Models;
 
 namespace TreatBox.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+      private readonly TreatBoxContext _db;
+
+      public HomeController(TreatBoxContext db)
+      {
+        _db = db;
+      }
+
+      public ActionResult Index()
+      {
+        var viewModel = new MyViewModel();
+        viewModel.AllTreats = _db.Treats.ToList();
+        viewModel.AllFlavors = _db.Flavors.ToList();
+        return View(viewModel);
+      }
 
     }
 }
---
+++ TreatBox/ViewModels/MyViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace TreatBox.Models
+{
+  public class MyViewModel
+  {
+    public List<Treat> AllTreats { get; set; }
+    public List<Flavor> AllFlavors { get; set; }
+  }
+} 
