811be18cfdf64fae8a73ce380cff4f9d91362277
taylulz <taylulzcode@gmail.com>
2021-01-15 09:51:22 -0800

    add non-auth index route to TreatsCntrlr

---
+++ TreatBox/Controllers/TreatsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System;
+using TreatBox.Models;
+
+namespace TreatBox.Controllers
+{
+  public class TreatsController : Controller
+  {
+    private readonly TreatBoxContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TreatsController(UserManager<ApplicationUser> userManager, TreatBoxContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public Task<ActionResult> Index()
+    {
+      List<Treat> userTreats = _db.Treats.ToList();
+      return View(userTreats);
+    }
+
+    
+  }
+}
