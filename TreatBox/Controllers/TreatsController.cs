0da081c2320e3d543a927941e073dcbd63db5c69
taylulz <taylulzcode@gmail.com>
2021-01-15 10:25:03 -0800

    working authorized Create GET/POST route/view

--- TreatBox/Controllers/TreatsController.cs
+++ TreatBox/Controllers/TreatsController.cs
@@ -29,6 +29,27 @@ namespace TreatBox.Controllers
       return View(userTreats);
     }
 
-    
+    [Authorize]
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      ViewBag.Flavors = _db.Flavors.ToList();
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      if(FlavorId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
