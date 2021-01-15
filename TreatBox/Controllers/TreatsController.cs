ca1fc880bc5248f1e76c128b8e04074b55f2fd21
taylulz <taylulzcode@gmail.com>
2021-01-15 14:12:05 -0800

    fix DeleteFlavor functionality treats side

--- TreatBox/Controllers/TreatsController.cs
+++ TreatBox/Controllers/TreatsController.cs
@@ -136,5 +136,14 @@ namespace TreatBox.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.TreatFlavor.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+      _db.TreatFlavor.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
